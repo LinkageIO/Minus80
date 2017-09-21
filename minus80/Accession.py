177aeacc17e56339c8984e788230dad4c0b6ec92
Rob <schae234@umn.edu>
2017-09-21 16:16:42 -0500

    simplify init process and add a repr method

--- minus80/Accession.py
+++ minus80/Accession.py
@@ -1,20 +1,29 @@
 
 class Accession(object):
-    def __init__(self,name,**kwargs):
+    '''
+        An Accession is an item that exists in an 
+        experimental collection. It can be a sample
+        or a tissue. Accessions have a set of files 
+        associated with them as well as a 
+    '''
+
+    def __init__(self,name,files=None,**kwargs):
         self.name = name
-        if 'files' in kwargs:
-            self.files = kwargs['files']
-            del kwargs['files']
+        if files != None:
+            self.files = files
         else:
-            self.files = []
+            self.files = set()
         self.metadata = kwargs
 
     def __getitem__(self,key):
         return self.metadata[key]
 
     def add_file(self,path):
-        self.files.append(path)
+        self.files.add(path)
 
     def add_files(self,paths):
         for path in paths:
             self.add_file(path)
+
+    def __repr__(self):
+        return f'Accession({self.name},files={self.files},{self.metadata})'
