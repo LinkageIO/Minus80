286ec5ffef9bfc5c2a4e9352c6e4f985d6bf0d33
Rob <schae234@umn.edu>
2017-09-22 11:15:13 -0500

    add base unit tests

---
+++ tests/test_Freezable.py
@@ -0,0 +1,58 @@
+import pytest
+import pandas as pd
+
+from minus80 import Cohort
+
+def test_guess_type(simpleCohort):
+    assert Cohort.guess_type(simpleCohort) == 'Cohort' 
+
+def test_dbfilename(simpleCohort):
+    mname = simpleCohort._m80_name
+    mtype = simpleCohort._m80_type
+    assert simpleCohort._dbfilename().endswith(f'{mtype}.{mname}.db')
+
+def test_open_db(simpleCohort):
+    simpleCohort._open_db(simpleCohort._m80_name)
+
+def test_store_bcolz(simpleCohort):
+    df = pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]],columns=['a','b','c']) 
+    simpleCohort._bcolz('testTable',df=df)
+
+def test_get_bcolz(simpleCohort):
+    df = pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]],columns=['a','b','c']) 
+    simpleCohort._bcolz('testTable',df=df)
+    df2 = simpleCohort._bcolz('testTable')
+    assert all(df == df2)
+    
+
+def test_tmpfile(simpleCohort):
+    tmpfile = simpleCohort._tmpfile()
+    a = open(tmpfile.name,'w')
+    print('test',file=a)
+    a.close()
+    b = open(tmpfile.name,'r')
+    assert b.readline().strip() == 'test'
+
+def test_dict(simpleCohort):
+    simpleCohort._dict('test','TEST')
+    assert simpleCohort._dict('test') == 'TEST'
+    try:
+        simpleCohort._dict('TEST')
+    except ValueError as e:
+        pass
+    assert True
+
+def test_m80_name(simpleCohort):
+    assert simpleCohort._m80_name == 'TestCohort'
+
+def test_m80_type(simpleCohort):
+    assert simpleCohort._m80_type == 'Cohort'
+
+def test_delete_m80():
+    import os
+    c = Cohort('DeleteMe')
+    dbFile = c._dbfilename()
+    assert os.path.exists(dbFile) == True
+    c._delete_m80()
+    assert os.path.exists(dbFile) == False
+
