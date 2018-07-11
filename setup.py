2bf69cd477e202f822d48029710e56f0dbbfca90
Rob Schaefer <schae234@umn.edu>
2018-07-11 12:49:53 -0600

    Remove step to install requirements in post develop

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ class PostDevelopCommand(develop):
     """
     def run(self):
         try:
-            print('Running post-installation for apsw')
-            check_call('pip install -r requirements.txt'.split())
+            #print('Running post-installation for apsw')
+            #check_call('pip install -r requirements.txt'.split())
             check_call('''\
 	    pip install --user https://github.com/rogerbinns/apsw/releases/download/3.22.0-r1/apsw-3.22.0-r1.zip \
 	    --global-option=fetch --global-option=--version --global-option=3.22.0 --global-option=--all \
