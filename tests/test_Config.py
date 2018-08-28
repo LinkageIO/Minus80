e2c0601159f2c6b9d3c4fee923823667e73191f4
rob <rob@linkage.io>
2018-08-28 11:12:51 -0600

    add config tests

---
+++ tests/test_Config.py
@@ -0,0 +1,21 @@
+import pytest
+from minus80.Config import cf,Level
+
+def test_get_attr():
+   cf.test = 'a'
+   assert cf.test == 'a'
+
+def test_get_item():
+    level = cf['options']
+
+def test_set_level_attr():
+    cf.test = Level()
+    cf.test.passed = True
+    assert cf.test.passed
+
+def test_get_cloud_endpoint():
+    assert cf.cloud.endpoint
+
+def test_pprint():
+    x=repr(cf)
+    assert True
