7963893f044c1ad799455493afcdbb68ade4fde8
rob <rob@linkage.io>
2018-09-25 17:12:11 -0600

    add requests dep

--- setup.py
+++ setup.py
@@ -75,7 +75,8 @@ setup(
         'click >= 6.7',
         'asyncssh >= 1.12.2',
         'networkx == 1.11',
-        'boto3>=1.7.84',
+        'boto3 >= 1.7.84',
+        'requests >= 2.19.1',
         'apsw'
     ],
     extras_require={
