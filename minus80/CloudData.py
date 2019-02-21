6e348add40a0195438e05602aa33b79462b7a530
Rob Schaefer <rob@linkage.io>
2019-02-21 12:11:05 -0700

    move the base code the CloudData.py

--- minus80/CloudData.py
+++ minus80/CloudData.py
@@ -10,5 +10,17 @@ def CloudData(engine='s3'):
     else:
         raise ValueError(f'Cannot use {engine} as a cloud engine.')
 
+class BaseCloudData(object): #pragma: no cover
+    def __init__(self):
+        pass
+
+    def push(self, name, dtype, raw=False, compress=False):
+        raise NotImplementedError('This engine does not support pushing')
+
+    def pull(self, name, dtype, raw=False):
+        raise NotImplementedError('This engine does not support pulling')
+
+    def list(self, name=None, dtype=None, raw=None):
+        raise NotImplementedError('This engine does not support listing')
 
 
