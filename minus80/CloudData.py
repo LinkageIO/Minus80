4e212d990dce82229ed35b51e74fbdb56796a5a3
Rob Schaefer <rob@linkage.io>
2019-02-25 14:59:01 -0700

    allow gcp engine

--- minus80/CloudData.py
+++ minus80/CloudData.py
@@ -1,10 +1,11 @@
 
-def CloudData(engine='s3'):
+def CloudData(engine='gcp'):
     if engine == 's3':
         from .S3CloudData import S3CloudData
         return S3CloudData()
     elif engine == 'gcp':
-        pass
+        from .GCPCloudData import GCPCloudData
+        return GCPCloudData()
     else:
         raise ValueError(f'Cannot use {engine} as a cloud engine.')
 
