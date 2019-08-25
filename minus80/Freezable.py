37e520f474ea5f3c2d68dc8beb404eae5a64bdde
Rob <rob@linkage.io>
2019-08-25 17:24:10 -0600

    add basic freeze/thaw functionality

--- minus80/Freezable.py
+++ minus80/Freezable.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import re
-import tempfile
+import shutil
 import hashlib
+import logging
+import tempfile
 
 import os as os
 import numpy as np
 import pandas as pd
 
-from shutil import rmtree as rmdir
-from tinydb import TinyDB
+from tinydb import Query
+from tinydb import where
 from pathlib import Path
-
+from tinydb import TinyDB
+from datetime import datetime
 
 from minus80.RelationalDB import relational_db
 from minus80.ColumnDB import columnar_db
@@ -19,9 +22,14 @@ from minus80.ColumnDB import columnar_db
 from .Config import cf
 from .Tools import guess_type
 
+from .Exceptions import (TagExistsError, 
+                        TagDoesNotExistError, 
+                        UnsavedChangesInThawedError)
+
 
 __all__ = ["Freezable"]
 
+log = logging.getLogger('minus80')
 
 class Freezable(object):
     """
@@ -40,13 +48,13 @@ class Freezable(object):
 
     """
 
-    def __init__(self, name, parent=None, basedir=None):
+    def __init__(self, name, basedir=None):
         """
             Freezable object inherit an insance to the Freezable API.
         """
         # Guess the dtype based on the class name
         dtype = guess_type(self)
-        self.m80 = FreezableAPI(dtype, name, parent, basedir)
+        self.m80 = FreezableAPI(dtype, name, basedir)
 
 
 class FreezableAPI(object):
@@ -58,37 +66,70 @@ class FreezableAPI(object):
         ----------
         name : str
             The name of the frozen object.
-        parent: Freezable object or None
-            The parent object
         """
         # Set the m80 name
-        self.name = name
+        self.name = self._validate_freezable_name(name)
         # Set the m80 dtype
-        self.dtype = dtype
-        self.tag = tag
+        self.dtype = self._validate_freezable_name(dtype)
 
         # Default to the basedir in the config file
         if basedir is None:
-            basedir = Path(cf.options.basedir).expanduser() / 'databases'
+            basedir = Path(cf.options.basedir).expanduser() / 'datasets'
         else:
             basedir = Path(basedir).expanduser()
         # Create the base dir
         self.basedir = basedir / self.slug
-        )
         os.makedirs(self.basedir, exist_ok=True)
+        os.makedirs(self.thawed_dir, exist_ok=True)
+        os.makedirs(self.frozen_dir, exist_ok=True)
+
         # Set up the columnar db
-        self.col = columnar_db(self.basedir)
+        self.col = columnar_db(self.thawed_dir)
         # Get a handle to the sql database
-        self.db = relational_db(self.basedir)
+        self.db = relational_db(self.thawed_dir)
         # Set up a table
-        self.doc = TinyDB(os.path.join(self.basedir, "tinydb.json"))
+        self.doc = TinyDB(os.path.join(self.thawed_dir, "tinydb.json"))
+
+
+    # Class Properties --------------------------------------------------
+
+    @property
+    def _manifest(self):
+        return TinyDB(self.basedir / 'MANIFEST.json')
 
     @property
     def slug(self):
-        if self.tag is not None:
-            return f'{self.dtype}.{self.name}:{self.tag}'
-        else:
-            return f'{self.dtype}.{self.name}'
+        return f'{self.dtype}.{self.name}'
+
+    @property
+    def thawed_dir(self):
+        return self.basedir / 'thawed'
+
+    @property
+    def frozen_dir(self):
+        return self.basedir / 'frozen'
+
+    @property
+    def tags(self):
+        '''
+            Returns the available frozen "tags"  
+        '''
+        tag_docs = self._manifest.search(where('tag').exists())
+        tags = set([x['tag'] for x in tag_docs])
+        # remove the "thawed" tag
+        tags.discard('thawed')
+        return tags
+
+    @property
+    def thawed_tag(self):
+        tag = self._manifest.get(where('tag') == 'thawed')
+        if tag is None:
+            tag = {
+                'tag' : 'thawed',
+                'parent' : None
+            }
+        #tag.update(self.checksum)
+        return tag
 
     @property
     def checksum(self):
@@ -96,7 +137,14 @@ class FreezableAPI(object):
         Calculates the checksum of all the files in the freezable 
         objects database directory   
         """
-        def update_hash(running_hash, filepath):
+        checksums = {
+            'slug': hashlib.sha256(
+                '{self.name}.{self.dtype}'.encode('utf-8')
+            ).hexdigest(),
+            'files': {},
+        }
+        def file_hash(filepath):
+            running_hash = hashlib.sha256()
             with open(filepath, 'rb') as IN:
                 while True:
                     # Read file in as little chunks.
@@ -104,14 +152,132 @@ class FreezableAPI(object):
                     if not buf:
                         break
                     running_hash.update(buf)
-        sha_hash = hashlib.sha256('{self.name}.{self.dtype}'.encode('utf-8'))
+            return running_hash.hexdigest()
         # iterate over the direcory and calucalte the hash
-        for root, dirs, files in os.walk(self.basedir):
-            for names in sorted(files):
-                filepath = os.path.join(root, names)
-                update_hash(running_hash=sha_hash,
-                            filepath=filepath)
-        return sha_hash.hexdigest()        
+        for root, dirs, files in os.walk(self.thawed_dir):
+            for file_path in sorted(files):
+                # Calculate a relative path to the freezable object
+                relative_path = str(Path(root) / file_path).replace(str(self.thawed_dir)+'/','')
+                full_file_path = Path(root) / file_path
+                # calculate and store the checksums
+                checksums['files'][relative_path] = file_hash(full_file_path)
+        # calculate the total
+        total = hashlib.sha256(checksums['slug'].encode('utf-8'))
+        for csum in checksums['files'].values():
+            total.update(csum.encode('utf-8'))
+        checksums['total'] = total.hexdigest()
+        return checksums
+
+    # Class Methods --------------------------------------------------
+
+    def freeze(self, tagname):
+        '''
+        Freezes tha current working (aka "thawed") dataset and freezes its contents 
+        for posterity. 
+        '''
+        # Create the tag from the current thawed tag
+        tag = self.thawed_tag
+        tagname = self._validate_tagname(tagname)
+        # Make sure the tag doesn't already exist
+        if tagname in self.tags:
+            raise TagExistsError(f'Tag already exists: {tagname}')
+        tag['tag'] = tagname 
+        # Update the tag with current file checksums
+        tag.update(self.checksum)
+
+        # Check to see what files need to be frozen
+        for path,phash in tag['files'].items():
+            if not (self.basedir / 'frozen' / phash).exists():
+                print(f'Found a new file: {path}')
+                shutil.copyfile(
+                    self.thawed_dir / path,
+                    self.frozen_dir / phash
+                )
+            else:
+                print(f'File already exists: {path}')
+
+        # Add a datetime to the document
+        tag['timestamp'] = str(datetime.now())
+
+        # Add the tag to the manifest
+        self._manifest.insert(tag)
+        # Update the current thawed tag
+        self._update_thawed_tag({'parent':tagname})
+
+    def thaw(self, tagname, force=False):
+        # Validate tag name
+        tagname = self._validate_tagname(tagname)
+        # Check that tag exists
+        tag_data = self._manifest.get(where('tag') == tagname)
+        if tag_data is None:
+            raise TagDoesNotExistError(f'{tagname} is not in frozen datasets')
+        # Check to see that current thawed dataset doesnt have unsaved work
+        current_checksum = self.checksum['total']        
+        parent_checksum = self._manifest.get(
+            where('tag') == self.thawed_tag['parent']
+        )['total']
+        if not force and current_checksum != parent_checksum:
+            raise UnsavedChangesInThawedError(
+                'freeze your current changes or use "force" to dispose '
+                'of any unsaved changes in current thawed dataset'
+            ) 
+        # Thaw it out ----------
+
+        # Remove the current files in the thawed directory 
+        shutil.rmtree(self.thawed_dir)
+        self.thawed_dir.mkdir(exist_ok=True)
+
+        for path,phash in tag_data['files'].items():
+            shutil.copyfile(
+                self.frozen_dir / phash,
+                self.thawed_dir / path,
+            )
+
+        self._update_thawed_tag({'parent':tagname})
+            
+
+    # Class internal methods---------------------------------------------
+
+    def _update_thawed_tag(self,doc=None):
+        '''
+            Updates the tag for "thawed" in the manifest
+        '''
+        tag = self.thawed_tag
+        #tag.update(self.checksum)
+        # Update core data
+        tag['timestamp'] = str(datetime.now())
+        # Update any extra data passed in through doc
+        if doc is not None:
+            tag.update(doc)
+        # Upsert the database
+        self._manifest.upsert(
+            tag,
+            where('tag') == 'thawed'
+        )
+
+    # Class static methods---------------------------------------------
+
+    @staticmethod
+    def _validate_freezable_name(name):
+        '''
+        Cannot contain slashes, periods, or colons
+        '''
+        import re
+        if re.search('[./:]+',name) is None:
+            return name
+        else:
+            raise ValueError(f'Invalid Freezable Name: "{name}"')
+
+    @staticmethod
+    def _validate_tagname(tagname):
+        '''
+        '''
+        import re
+        if re.search('[:]+',tagname) is None:
+            return tagname
+        else:
+            raise ValueError(f'Invalid Tag Name: "{tagname}"')
+
 
     @staticmethod
     def tmpfile(*args, **kwargs):
