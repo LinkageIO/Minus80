ecdcc8b2781cf555f4d39094cf66f25c101c7e7a
Rob <rob@linkage.io>
2019-09-10 09:49:26 -0600

    add tag exceptions

--- minus80/Exceptions.py
+++ minus80/Exceptions.py
@@ -1,12 +1,18 @@
 
 class M80Error(Exception):
-    def __init__(self,msg):
+    def __init__(self,msg=''):
         self.message = msg
 
 class TagExistsError(M80Error):
     pass
 
 class TagDoesNotExistError(M80Error):
+    pass
+
+class TagInvalidError(M80Error):
+    pass
+
+class FreezableNameInvalidError(M80Error):
     pass
 
 class UnsavedChangesInThawedError(M80Error):
