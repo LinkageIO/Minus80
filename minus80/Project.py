8cd92d39748d5dd9864d400f8730b9ce7d7d4a8c
rob <rob@linkage.io>
2019-09-11 12:03:26 -0600

    make this much simpler

--- minus80/Project.py
+++ minus80/Project.py
@@ -1,6 +1,4 @@
-import os
-import pathlib
-
+from pathlib import Path
 from tinydb import where
 from minus80 import Freezable
 
@@ -8,38 +6,11 @@ class Project(Freezable):
 
     def __init__(self, name, basedir=None):
         super().__init__(name,basedir=basedir)
+        # create the internal data dir 
+        (self.m80.thawed_dir / 'data').mkdir(exist_ok=True)
 
-        # do some checks
-        if self.dir is not None:
-            if not self.symdir.exists():
-                self.symdir.symlink_to(self.dir)
-
-    @property
-    def symdir(self):
-        return self.m80.thawed_dir / 'symdir'
-
-    @property   
-    def dir(self):
-        doc = self.m80.doc.get(where('dir').exists())
-        if doc is None:
-            return doc
-        else:
-            return pathlib.Path(doc['dir'])
-
-    @dir.setter
-    def dir(self,dir):
-        if self.dir is not None:
-            raise ValueError('Cannot change project dir')
-        dir = pathlib.Path(dir).resolve()
-        if not dir.exists():
-            raise ValueError(f'directory "{dir}" does not exist')
-        self.m80.doc.upsert(
-            {'dir':str(dir)},
-            where('dir').exists()
-        )
-        # symlink to project dir
-        (self.m80.thawed_dir/'symdir').symlink_to(dir)
-        
-        
-
-
+    def create_link(self,path):
+        path = Path(path)
+        if path.exists():
+            raise ValueError(f'"{path}" already exists.')
+        path.symlink_to(self.m80.thawed_dir/'data')
