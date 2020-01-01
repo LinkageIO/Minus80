21e5c5413af4ccf4eec8da9a2f2c6a652fca3aa1
Rob <rob@linkage.io>
2020-01-01 14:54:15 -0700

    bump code cov

--- tests/test_Cohort.py
+++ tests/test_Cohort.py
@@ -87,3 +87,80 @@ def test_from_accessions():
     c = Accession("Sample3", files=["file1.txt", "file2.txt"], type="CHIP")
     d = Accession("Sample4", files=["file1.txt", "file2.txt"], type="CHIP")
     x = Cohort.from_accessions("TestCohort", [a, b, c, d])
+
+def test_get_columsn(simpleCohort):
+    assert 'type' in simpleCohort.columns
+
+def test_get_names(simpleCohort):
+    assert ['Sample1', 'Sample2', 'Sample3', 'Sample4'] == simpleCohort.names
+
+def test_get_raw_files(simpleCohort):
+    assert ['file1.txt', 'file2.txt'] == simpleCohort.raw_files
+
+def test_ignored_files(simpleCohort):
+    assert [] == simpleCohort.ignored_files
+
+def test_num_files(simpleCohort):
+    assert simpleCohort.num_files == 2
+
+def test_as_DataFrame(simpleCohort):
+    import pandas as pd
+    assert isinstance(simpleCohort.as_DataFrame(), pd.DataFrame)
+
+def test_get_fileinfo(simpleCohort):
+    assert simpleCohort.get_fileinfo('file1.txt').url == 'file1.txt'
+
+def test_add_accession_by_df(simpleCohort):
+    import pandas as pd
+    df = pd.DataFrame( 
+        [['S1', 23, 'O'],
+         ['S2', 30, 'O+']],
+         columns =  ['Name','Age','Type']
+    )
+    simpleCohort.add_accessions_from_DataFrame(df,'Name')
+    
+def test_add_accession_by_df_bad_name(simpleCohort):
+    import pandas as pd
+    df = pd.DataFrame( 
+        [['S1', 23, 'O'],
+         ['S2', 30, 'O+']],
+         columns =  ['Name','Age','Type']
+    )
+    with pytest.raises(ValueError):
+        simpleCohort.add_accessions_from_DataFrame(df,'BAD')
+
+def test_add_accession_by_df_delete_missing_Data(simpleCohort):
+    import pandas as pd
+    import numpy as np
+    # Minus80 will dele the Age value for S1
+    df = pd.DataFrame( 
+        [['S1', np.nan, 'O'],
+         ['S2', 30, 'O+']],
+         columns =  ['Name','Age','Type']
+    )
+    simpleCohort.add_accessions_from_DataFrame(df,'Name')
+
+def test_search_files(simpleCohort):
+    assert simpleCohort.search_files('file1') == ['file1.txt']
+
+def test_search_accessions(simpleCohort):
+    assert simpleCohort.search_accessions('Sample') == ['Sample1', 'Sample2', 'Sample3', 'Sample4']
+
+def test_search_metadata(simpleCohort):
+    assert len(simpleCohort.search_metadata(type='WGS')) == 2
+
+def test_add_raw_file(simpleCohort):
+    simpleCohort.add_raw_file('test.txt')
+    
+def test_add_raw_file_relative_fails(simpleCohort):
+    with pytest.raises(ValueError):
+        simpleCohort.add_raw_file('./test.txt')
+
+def test_get_name(simpleCohort):
+    assert simpleCohort.get_name('Sample1') == 'Sample1'
+
+def test_get_name_from_AID(simpleCohort):
+    assert simpleCohort.get_name(simpleCohort['Sample1']['AID']) == 'Sample1'
+
+def test_get_aliases(simpleCohort):
+    assert simpleCohort.get_aliases('Sample1')
